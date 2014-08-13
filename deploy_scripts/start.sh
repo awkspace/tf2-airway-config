032ce89d5d0f5a4ddf00942cfd9160af6346e3f1
awkisopen <awkisopen@shutupandwrite.net>
2014-08-13 19:09:11 -0400

    start.sh: Add rcon support

--- deploy_scripts/start.sh
+++ deploy_scripts/start.sh
@@ -1,1 +1,2 @@
-tf2/srcds_run -game tf +map $(cat /home/gameserver/tf2/tf/airway_version.txt).bsp -autoupdate -steam_dir /home/gameserver -steamcmd_script /home/gameserver/tf2_ds.txt
+PUBLIC_IP=$(wget http://ipecho.net/plain -O - -q ; echo)
+tf2/srcds_run -game tf +map $(cat /home/gameserver/tf2/tf/airway_version.txt).bsp -autoupdate -steam_dir /home/gameserver -steamcmd_script /home/gameserver/tf2_ds.txt -ip $PUBLIC_IP +rcon_password $(cat /home/gameserver/rcon_password.txt)
