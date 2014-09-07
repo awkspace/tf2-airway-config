462f4bc9e4787c1add1dad8b51fe6b046ed89685
awkisopen <awkisopen@shutupandwrite.net>
2014-09-07 10:29:54 -0400

    Decrease maxplayers to 23 (22 with Replay)

--- deploy_scripts/start.sh
+++ deploy_scripts/start.sh
@@ -1,2 +1,2 @@
 PUBLIC_IP=$(wget http://ipecho.net/plain -O - -q ; echo)
-tf2/srcds_run -game tf +map $(cat /home/gameserver/tf2/tf/airway_version.txt).bsp -autoupdate -steam_dir /home/gameserver -steamcmd_script /home/gameserver/tf2_ds.txt -ip $PUBLIC_IP +rcon_password $(cat /home/gameserver/rcon_password.txt)
+tf2/srcds_run -game tf +map $(cat /home/gameserver/tf2/tf/airway_version.txt).bsp -autoupdate -steam_dir /home/gameserver -steamcmd_script /home/gameserver/tf2_ds.txt -ip $PUBLIC_IP +rcon_password $(cat /home/gameserver/rcon_password.txt) +maxplayers 23
